0a343653f0a155a0f0c0424ab478feed89fe3aff
senceryazici <sencer_yazici98@hotmail.com>
2017-04-19 13:40:44 +0300

    Initial Commit

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.camStatLabel = new System.Windows.Forms.Label();
+            this.parameterspanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.artHorpanel = new System.Windows.Forms.PictureBox();
+            this.closeButton = new System.Windows.Forms.PictureBox();
+            this.toptitlebar1 = new System.Windows.Forms.PictureBox();
+            this.consoleBox = new System.Windows.Forms.RichTextBox();
+            this.parameterspanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artHorpanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toptitlebar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // camStatLabel
+            // 
+            this.camStatLabel.AutoSize = true;
+            this.camStatLabel.BackColor = System.Drawing.Color.Transparent;
+            this.camStatLabel.ForeColor = System.Drawing.Color.Black;
+            this.camStatLabel.Location = new System.Drawing.Point(1072, 140);
+            this.camStatLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.camStatLabel.Name = "camStatLabel";
+            this.camStatLabel.Size = new System.Drawing.Size(40, 17);
+            this.camStatLabel.TabIndex = 9;
+            this.camStatLabel.Text = "1234";
+            // 
+            // parameterspanel
+            // 
+            this.parameterspanel.Controls.Add(this.button1);
+            this.parameterspanel.Controls.Add(this.label3);
+            this.parameterspanel.Controls.Add(this.textBoxPort);
+            this.parameterspanel.Controls.Add(this.label2);
+            this.parameterspanel.Controls.Add(this.textBoxIP);
+            this.parameterspanel.Location = new System.Drawing.Point(825, 266);
+            this.parameterspanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.parameterspanel.Name = "parameterspanel";
+            this.parameterspanel.Size = new System.Drawing.Size(447, 675);
+            this.parameterspanel.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(267, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Port:";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(313, 14);
+            this.textBoxPort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(41, 22);
+            this.textBoxPort.TabIndex = 11;
+            this.textBoxPort.Text = "8091";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(65, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "IP Address:";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(155, 14);
+            this.textBoxIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(103, 22);
+            this.textBoxIP.TabIndex = 9;
+            this.textBoxIP.Text = "192.168.2.127";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication5.Properties.Resources.paramtab_000003;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 207);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(685, 951);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // artHorpanel
+            // 
+            this.artHorpanel.Location = new System.Drawing.Point(753, 219);
+            this.artHorpanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.artHorpanel.Name = "artHorpanel";
+            this.artHorpanel.Size = new System.Drawing.Size(1280, 886);
+            this.artHorpanel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.artHorpanel.TabIndex = 8;
+            this.artHorpanel.TabStop = false;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Image = global::WindowsFormsApplication5.Properties.Resources.Comp_2_00000_00000;
+            this.closeButton.Location = new System.Drawing.Point(2349, 15);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(64, 59);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.TabStop = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // toptitlebar1
+            // 
+            this.toptitlebar1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.toptitlebar1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.toptitlebar1.BackColor = System.Drawing.Color.Transparent;
+            this.toptitlebar1.Image = global::WindowsFormsApplication5.Properties.Resources.Comp_1_000002;
+            this.toptitlebar1.Location = new System.Drawing.Point(648, 15);
+            this.toptitlebar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.toptitlebar1.Name = "toptitlebar1";
+            this.toptitlebar1.Size = new System.Drawing.Size(1423, 59);
+            this.toptitlebar1.TabIndex = 0;
+            this.toptitlebar1.TabStop = false;
+            this.toptitlebar1.Click += new System.EventHandler(this.toptitlebar1_Click);
+            // 
+            // consoleBox
+            // 
+            this.consoleBox.BackColor = System.Drawing.Color.RoyalBlue;
+            this.consoleBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.consoleBox.Location = new System.Drawing.Point(455, 55);
+            this.consoleBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.consoleBox.Name = "consoleBox";
+            this.consoleBox.ReadOnly = true;
+            this.consoleBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.consoleBox.Size = new System.Drawing.Size(132, 117);
+            this.consoleBox.TabIndex = 11;
+            this.consoleBox.Text = "";
+            this.consoleBox.TextChanged += new System.EventHandler(this.consoleBox_TextChanged);
+            this.consoleBox.MouseEnter += new System.EventHandler(this.consoleBox_MouseEnter);
+            this.consoleBox.MouseLeave += new System.EventHandler(this.consoleBox_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(1924, 1084);
+            this.ControlBox = false;
+            this.Controls.Add(this.consoleBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.parameterspanel);
+            this.Controls.Add(this.camStatLabel);
+            this.Controls.Add(this.artHorpanel);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.toptitlebar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(1707, 886);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "v";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.parameterspanel.ResumeLayout(false);
+            this.parameterspanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artHorpanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toptitlebar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox toptitlebar1;
+        private System.Windows.Forms.PictureBox closeButton;
+        private System.Windows.Forms.PictureBox artHorpanel;
+        private System.Windows.Forms.Label camStatLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel parameterspanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxIP;
+        public System.Windows.Forms.RichTextBox consoleBox;
+    }
+}
+
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using System.Net.Sockets;
+using System.Net;
+
+//THIS SOFTWARE HAS BEEN DEVELOPED BY SENCER YAZICI. FOR MORE INFORMATION PLASE CONTACT VIA EMAIL "sencer_yazici98@hotmail.com" 
+// NOTE: THIS SOFTWARE IS IN DEVELOPMENT UNTER ITUROV TEAM AT ISTANBUL TECHNICAL UNIVERSITY
+// VISIT GITHUB PAGE: https://github.com/iturov
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MJPEGStream stream;
+        int statePanel1 = 0;
+        Point parameterBase;
+        Point panelbase;
+        public static string[] dataReceived = new string[32];
+        public static string[] dataSent = new string[32];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cameraInitialize();
+            graphicsInitalize();  
+        }
+
+        void newFrame(object sender, NewFrameEventArgs eventargs)
+        {
+            Bitmap bmp = (Bitmap)eventargs.Frame.Clone();
+            artHorpanel.Image = bmp;
+        } // FOR CAPTURING NEW FRAMES
+
+        private void closeButton_Click(object sender, EventArgs e) // CLOSE BUTTON 
+        {
+            //stream.SignalToStop();
+            stream.Stop();
+            Application.Exit();
+            
+        }
+
+        private void toptitlebar1_Click(object sender, EventArgs e)// WEBSITE
+        {
+            System.Diagnostics.Process.Start("https://github.com/iturov");
+        }
+
+        private void button1_Click(object sender, EventArgs e) //STOP , CHANGE PARAMETERS ,RESTART
+        {
+            stream.Stop();
+            stream.Source ="http://" + textBoxIP.Text + ":" + textBoxPort.Text + "/?action=stream";
+            stream.Start();
+            if (stream.IsRunning)
+            {
+                camStatLabel.Text = "Receiving Video Feed From:   " + "http://" + textBoxIP.Text + ":" + textBoxPort.Text + "/?action=stream";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)//IMPORTANT, STREAM HAS TO BE STOPPED BEFORE QUITTING APPLICATION
+        {
+            //stream.SignalToStop();
+            //stream.Stop();//STOPPING STREAM
+
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            int widthP = pictureBox1.Width - 50; //PANELS WIDTH
+            statePanel1 += 1; // ON/OFF STATE
+            if (statePanel1 >= 2) statePanel1 = 0;
+
+            if (statePanel1 == 1) // PANEL IS CLOSED, NOW OPENING 
+            {
+                for (int i = 0; i < widthP; i++)
+                {
+                    parameterBase.X++;
+                    panelbase.X++;
+                    parameterspanel.Location = panelbase;
+                    pictureBox1.Location = parameterBase; //PANEL'S POSITION INCREASES
+                }
+                    
+            }
+            else // PANEL IS OPEN, NOW CLOSING
+            {
+                for (int i = 0; i < widthP; i++)
+                {
+                    parameterBase.X--;
+                    panelbase.X--;
+                    parameterspanel.Location = panelbase;
+                    pictureBox1.Location = parameterBase;//PANEL'S POSITION DECREASES
+                }
+                    
+            }
+        }
+
+        private void consoleBox_MouseEnter(object sender, EventArgs e)
+        {
+            consoleBox.BorderStyle = BorderStyle.FixedSingle;
+            //consoleBox.BackColor = Color.FromArgb(255, 12, 12, 12);
+        }
+
+        private void consoleBox_MouseLeave(object sender, EventArgs e)
+        {
+            consoleBox.BorderStyle = BorderStyle.None;
+        }
+
+        private void consoleBox_TextChanged(object sender, EventArgs e)
+        {
+            consoleBox.SelectionStart = consoleBox.Text.Length;
+            // scroll it automatically
+            consoleBox.ScrollToCaret();
+        }
+        
+        private void graphicsInitalize()
+        {
+            /***** GRAPHICS INITIALIZE *****/
+            this.WindowState = FormWindowState.Maximized;
+            /*** DECLARE THIS VARIABLE ***/
+            //string path1 = "";
+            //toptitlebar1.ImageLocation = path1;
+            toptitlebar1.Width = toptitlebar1.Image.Width;
+            toptitlebar1.Height = toptitlebar1.Image.Height;
+            toptitlebar1.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - toptitlebar1.Width / 2, 0);
+            toptitlebar1.BackColor = Color.Transparent;
+            /*** DECLARE THIS VARIABLE ***/
+            //string path1 = "";
+            //toptitlebar1.ImageLocation = path1;
+            closeButton.Width = closeButton.Image.Width;
+            //Screen.PrimaryScreen.
+            closeButton.Height = closeButton.Image.Height;
+            closeButton.Location = new Point(Screen.PrimaryScreen.Bounds.Width - closeButton.Width, 0);
+            parameterBase = new Point(225 - pictureBox1.Width, Screen.PrimaryScreen.Bounds.Height / 2 - pictureBox1.Height / 2);//CHANGED
+            pictureBox1.Location = parameterBase;
+            panelbase = new Point(-parameterspanel.Width, Screen.PrimaryScreen.Bounds.Height / 2 - pictureBox1.Height / 2 + 35);//CHANGED
+            parameterspanel.Location = panelbase;
+            camStatLabel.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - camStatLabel.Width / 2, toptitlebar1.Height - 20);
+            //artHorpanel.Location = new Point(Screen.PrimaryScreen.Bounds.Width/2 - artHorpanel.Width/2, Screen.PrimaryScreen.Bounds.Height/2 - artHorpanel.Height/2);
+            consoleBox.Size = new Size(Screen.PrimaryScreen.Bounds.Width, 150);
+            consoleBox.Location = new Point(0, Screen.PrimaryScreen.Bounds.Height - consoleBox.Height);
+            consoleBox.BackColor = Color.FromArgb(255, 12, 12, 12);
+            //consoleBox.ForeColor = Color.FromArgb(255,89,124,230);
+            consoleBox.BorderStyle = BorderStyle.None;
+            consoleBox.ForeColor = Color.White;
+            consoleBox.BringToFront();
+            consoleBox.Parent = artHorpanel;
+
+            artHorpanel.Location = new Point(0, 0);
+            artHorpanel.Size = new Size(1920, 1080);
+            artHorpanel.SendToBack();
+            pictureBox1.Width = pictureBox1.Image.Width;
+            pictureBox1.Height = pictureBox1.Image.Height;
+
+            camStatLabel.BackColor = Color.Transparent;
+            toptitlebar1.BackColor = Color.Transparent;
+            closeButton.BackColor = Color.Transparent;
+            pictureBox1.BackColor = Color.Transparent;
+            camStatLabel.Parent = artHorpanel;
+            pictureBox1.Parent = artHorpanel;
+            parameterspanel.BringToFront();
+            //parameterspanel.Parent = pictureBox1;
+            toptitlebar1.Parent = artHorpanel;
+            closeButton.Parent = artHorpanel;
+            //consoleBox.Parent = artHorpanel;
+
+            /***** GRAPHICS INITIALIZE *****/
+        }
+
+        private void cameraInitialize()
+        {
+            /***** CAMERA INITIALIZE *****/
+            //stream = new MJPEGStream("http://192.168.2.127:8091/?action=stream");
+            stream = new MJPEGStream("http://" + textBoxIP.Text + ":" + textBoxPort.Text + "/?action=stream");
+            stream.NewFrame += new NewFrameEventHandler(newFrame);
+
+            camStatLabel.Text = "Receiving Video Feed From:   " + "http://" + textBoxIP.Text + ":" + textBoxPort.Text + "/?action=stream";
+            /***** CAMERA INITIALIZE *****/
+        }
+
+        public void myConsole(string output)
+        {
+            //Console.WriteLine(output);
+            consoleBox.AppendText("\r\n" + output);
+        }
+
+        public static void Listen()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 11000;
+                IPAddress localAddr = IPAddress.Any;
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    //myConsole("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    //myConsole("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        //myConsole("Received:" + data);
+                        string[] receivedString = data.Split(',');
+                        for(int k = 0; k < receivedString.Length; k++)
+                        {
+                            Form1.dataReceived[k] = receivedString[k];
+                        }
+
+                        string outgoingData = dataSent[3] + "," + dataSent[4] + "," + dataSent[5];
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(outgoingData);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+
+                        //myConsole("Sent:" + data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
